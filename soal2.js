c95f9127fa294beffb763657ff511f4cd4e57fe8
Adit <adityafauzanirsyad@gmail.com>
2019-09-28 17:21:53 +0700

    soal nomer 2

---
+++ soal2.js
@@ -0,0 +1,16 @@
+function username(data){
+    if (data.length >= 6){
+        console.log(`hai user : ${data}`)
+    }else {
+        console.log('usernama harus lebih dari 6')
+    }}
+
+function password(data){
+    if (data.length >= 7){
+        console.log(`password berhasil : ${data}`)
+    }else{
+        console.log('password harus lebih dari 7')
+    }
+}
+username ('bjuiubhjbb')
+password ('hahahahaha')
